9c30e318d4e308dce174da90000af034fe9974f2
Wasif Hasan <wasifhasancse@gmail.com>
2026-06-24 12:14:24 +0600

    fixed signup page path

--- src/app/(private)/signup/page.jsx
+++ src/app/(private)/signup/page.jsx
@@ -1,12 +1,14 @@
 import { Suspense } from "react";
 import {Spinner} from "@heroui/react";
+import SignUpForm from "./SignUpForm";
 
 export default function Page() {
   return (
     <Suspense fallback={<div className="flex flex-col items-center gap-2">
         <Spinner size="lg" />
         <span className="text-xs text-muted">Large</span>
-      </div>}>
+    </div>}>
+      <SignUpForm/>
     </Suspense>
   );
 }
