faba10c2422a8695a87c6b1b7ddd09c712a1e351
Wasif Hasan <wasifhasancse@gmail.com>
2026-06-24 12:16:29 +0600

    set sign in page

--- src/app/(private)/signup/page.jsx
+++ src/app/(private)/signup/page.jsx
@@ -1,14 +1,20 @@
+import { Spinner } from "@heroui/react";
 import { Suspense } from "react";
-import {Spinner} from "@heroui/react";
 import SignUpForm from "./SignUpForm";
 
-export default function Page() {
+const SignUpPage = () => {
   return (
-    <Suspense fallback={<div className="flex flex-col items-center gap-2">
-        <Spinner size="lg" />
-        <span className="text-xs text-muted">Large</span>
-    </div>}>
-      <SignUpForm/>
+    <Suspense
+      fallback={
+        <div className="flex flex-col items-center gap-2">
+          <Spinner size="lg" />
+          <span className="text-xs text-muted">Large</span>
+        </div>
+      }
+    >
+      <SignUpForm />
     </Suspense>
   );
-}
+};
+
+export default SignUpPage;
