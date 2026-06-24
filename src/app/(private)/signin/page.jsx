dcf72b354f240cffdcf204af97705a7b6ae0872f
Wasif Hasan <wasifhasancse@gmail.com>
2026-06-24 12:23:41 +0600

    fixed sign in form path

--- src/app/(private)/signin/page.jsx
+++ src/app/(private)/signin/page.jsx
@@ -1,19 +1,18 @@
 import { Spinner } from "@heroui/react";
 import { Suspense } from "react";
+import SignInForm from "./SignInForm";
 const SignInPage = () => {
   return (
     <Suspense
-          fallback={
-            <div className="flex flex-col items-center gap-2">
-              <Spinner size="lg" />
-              <span className="text-xs text-muted">Large</span>
-            </div>
-          }
-        >
-
-
-
-        </Suspense>
+      fallback={
+        <div className="flex flex-col items-center gap-2">
+          <Spinner size="lg" />
+          <span className="text-xs text-muted">Large</span>
+        </div>
+      }
+    >
+      <SignInForm />
+    </Suspense>
   );
 };
 
